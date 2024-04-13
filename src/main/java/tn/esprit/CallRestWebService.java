2d48fe475e1df178c051e90c320704582ed6f44d
khalil <97963608+AKM-FR@users.noreply.github.com>
2024-04-13 16:12:29 +0100

    2nd commit

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -20,7 +20,7 @@ import org.json.JSONObject;
  */
 
 public class CallRestWebService {
-
+// Test for 2nd commit
 	public static final String endpoint = "http://ip-api.com/json";
 	//public static final String endpoint = "https://httpbin.org/get";
 	public static void main(String[] args) {
